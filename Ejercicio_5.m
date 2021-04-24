4bd4d357e9d568529748cd787cd5b4e5be1ac373
Sofía Acerbi Martini <36568283+sofiacerbi@users.noreply.github.com>
2021-04-24 14:33:07 +0200

    Rename Ejercicio5.m to Ejercicio_5.m

--- Ejercicio_5.m
+++ Ejercicio_5.m
@@ -19,7 +19,7 @@ grid on
 z1 = [-3 -0.1]; 		            %Define los ceros de Gc(s)
 p1 = [-26.6656 -0.01]; 		            %Define los polos de Gc(s)
 k1 = [86.7878];                  %Define la ganancia de Gc(s)
-Gc2 = zpk(z1,p1,k1) 		    %Crea la funci髇 de transferencia equivalente
+Gc2 = zpk(z1,p1,k1) 		    %Crea la funci贸n de transferencia equivalente
 GcG2=G*Gc2
 GcGH2 = feedback(GcG2,H)        
 step(GcGH2)
@@ -37,7 +37,7 @@ c=step(GH1,t);
 c2=step(GcGH2,t);
 figure(3)
 plot(t,c,'r',t,c2,'g')
-title('Respuesta de escal髇')
+title('Respuesta de escal贸n')
 legend({'Sistema sin compensar ','Sistema compensado)'},'Location','southwest')
 grid on
 
@@ -45,7 +45,7 @@ grid on
 z2 = [-8.6169 -0.1]; 		            %Define los ceros de Gc(s)
 p2 = [-40.28 -0.0214]; 		            %Define los polos de Gc(s)
 k2 = [97.6505];                  %Define la ganancia de Gc(s)
-Gc3 = zpk(z2,p2,k2) 		    %Crea la funci髇 de transferencia equivalente
+Gc3 = zpk(z2,p2,k2) 		    %Crea la funci贸n de transferencia equivalente
 GcG3=G*Gc3
 GcGH3 = feedback(GcG3,H)        
 step(GcGH3)
@@ -63,7 +63,7 @@ c=step(GH1,t);
 c3=step(GcGH3,t);
 figure(5)
 plot(t,c,'r',t,c3,'g')
-title('Respuesta de escal髇')
+title('Respuesta de escal贸n')
 legend({'Sistema sin compensar ','Sistema compensado)'},'Location','southwest')
 grid on
 
