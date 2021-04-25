26763710d6a3fc7900c4be70d7b6d411e2dd7668
Sofía Acerbi Martini <36568283+sofiacerbi@users.noreply.github.com>
2021-04-25 21:36:05 +0200

    Rename Ejercicio5.m to Ejercicio_5.m

--- Ejercicio_5.m
+++ Ejercicio_5.m
@@ -11,7 +11,7 @@ figure(1)
 step(GH1)
 xlabel('Tiempo') 
 ylabel('Amplitud') 
-title('Respuesta del escal髇')
+title('Respuesta del escal贸n')
 grid on
 t=0:0.05:6;
 s=tf([1],[1 0])
@@ -25,7 +25,7 @@ grid on
 z1 = [-3 -0.1]; 		            %Define los ceros de Gc(s)
 p1 = [-26.6656 -0.01]; 		            %Define los polos de Gc(s)
 k1 = [86.7878];                  %Define la ganancia de Gc(s)
-Gc2 = zpk(z1,p1,k1) 		    %Crea la funci髇 de transferencia equivalente
+Gc2 = zpk(z1,p1,k1) 		    %Crea la funci贸n de transferencia equivalente
 GcG2=G*Gc2
 GcGH2 = feedback(GcG2,H)  
 K = 1; 
@@ -46,7 +46,7 @@ figure(4)
 plot(t,c,'r',t,c2,'g')
 xlabel('Tiempo (s)') 
 ylabel('Amplitud')
-title('Respuesta de escal髇')
+title('Respuesta de escal贸n')
 legend({'Sistema sin compensar ','Sistema compensado)'},'Location','southwest')
 grid on
 
@@ -54,7 +54,7 @@ grid on
 z2 = [-8.6169 -0.1]; 		            %Define los ceros de Gc(s)
 p2 = [-40.28 -0.0214]; 		            %Define los polos de Gc(s)
 k2 = [97.6505];                  %Define la ganancia de Gc(s)
-Gc3 = zpk(z2,p2,k2) 		    %Crea la funci髇 de transferencia equivalente
+Gc3 = zpk(z2,p2,k2) 		    %Crea la funci贸n de transferencia equivalente
 GcG3=G*Gc3
 GcGH3 = feedback(GcG3,H)
 K = 1; 
@@ -75,7 +75,7 @@ figure(6)
 plot(t,c,'r',t,c3,'g')
 xlabel('Tiempo (s)') 
 ylabel('Amplitud')
-title('Respuesta de escal髇')
+title('Respuesta de escal贸n')
 legend({'Sistema sin compensar ','Sistema compensado)'},'Location','southwest')
 grid on
 
